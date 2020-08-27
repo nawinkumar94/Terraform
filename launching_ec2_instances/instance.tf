a683224ef15c81a3306696c312bdc687f18c3633
nawinkumar94 <nawinkumar94@gmail.com>
2020-08-27 10:51:08 +0530

    correction commit

--- launching_ec2_instances/instance.tf
+++ launching_ec2_instances/instance.tf
@@ -1,6 +1,11 @@
 resource "aws_instance" "ec2_example_1"{
   ami = var.AMIS[var.AWS_REGION_NAME]
   instance_type = "t2.micro"
+
+  # This will run only in linux or mac not windows, because it is using local-exec not remote-exec
+  provisioner "local-exec"{
+      command = "echo ${aws_instance.ec2_example_1.private_ip} >> privateip.txt"
+  }
 }
 
 output "ip"{
